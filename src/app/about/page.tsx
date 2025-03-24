6c7f515bfa0fadbaad2e6182732ea3561e515be5
Venkatesh kumar <167079967+Venky6520@users.noreply.github.com>
2025-03-24 19:06:05 +0530

    to generate static params

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,10 @@
-export default function About(){
+import { cookies } from "next/headers";
+
+export default async function About(){
+    const cookieStore =await cookies()
+const theme=cookieStore.get("theme") 
+console.log(theme);
+
     console.log("server component rendered");
 
 
